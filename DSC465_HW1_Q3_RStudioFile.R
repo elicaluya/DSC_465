e58c048ab92d8f0aff0c3d7cdfe115ed382471b3
Elijah Caluya <elicaluya@yahoo.com>
2021-04-07 23:41:18 -0700

    adding files for questions 1-3 for HW1

---
+++ DSC465_HW1_Q3_RStudioFile.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+
+# Loading in Infant Data
+InfantData1 <- read.csv("C:/Users/elica/Desktop/depaul_grad/DSC_465/InfantData1.csv")
+
+# A) Scatterplot for Height and Weight differentiated by Sex
+ggplot(data=InfantData1, aes(x=HeightIn, y=WeightLbs, color=Sex)) + geom_point()
